4c8becb1bd5ae30e801aa05466b53baf8641eef5
LucasManza <manzanelli95lucas@gmail.com>
2018-04-09 20:30:58 -0300

    First push Login and Register New User

--- ui/src/app/new-user/new-user.component.ts
+++ ui/src/app/new-user/new-user.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-user.component.scss']
 })
 export class NewUserComponent implements OnInit {
-
+  title = "Nuevo Usuario";
   constructor() { }
 
   ngOnInit() {
