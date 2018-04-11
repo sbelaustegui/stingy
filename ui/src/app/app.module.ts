dda13bd6877ec2ee49ce4ddabae92e4299ed4082
LucasManza <manzanelli95lucas@gmail.com>
2018-04-11 13:57:55 -0300

    Login Reactive Forms

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import {AppRoutingModule} from "./app-routing.module";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import { LoginComponent } from './login/login.component';
 import { NewUserComponent } from './new-user/new-user.component';
@@ -27,6 +27,7 @@ import {CookieService} from "angular2-cookie/core";
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers : [
--- ui/src/app/login/login.component.ts
+++ ui/src/app/login/login.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+import {UserAuthService} from "../shared/auth/user/user-auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [UserAuthService]
 })
 export class LoginComponent implements OnInit {
 
-  title = "Stingy";
-  constructor() { }
+  public formGroup: FormGroup;
+  public loginUser;
+  public loginUserError: boolean;
+
+
+  constructor(public formBuilder: FormBuilder, public authUserService: UserAuthService, public router: Router) {}
 
   ngOnInit() {
+    this.loginUser = LoginUser.empty();
+    this.formGroup = this.createFormGroup();
+    this.loginUserError = false;
   }
+
+  login() {
+    this.authUserService.login(this.loginUser).then(res => {
+      this.router.navigate(['user','home'])
+    }).catch(err => this.loginUserError = true);
+  }
+
+  private createFormGroup() {
+    return this.formBuilder.group(
+      {
+        username: ['', Validators.required],
+        password: ['', Validators.required, Validators.minLength(8)],
+      }
+    );
+  }
+
 
 }
