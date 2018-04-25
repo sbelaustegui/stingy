a1459d4553f9dfe1cb062ebea0db5660097d5934
Sebas-Sirius <sebasbelaustegui@sirius.com.ar>
2018-04-25 11:00:34 -0300

    categories and subcategories admin added

--- ui/src/app/admin/admin-user/categories/categories.component.ts
+++ ui/src/app/admin/admin-user/categories/categories.component.ts
@@ -60,6 +60,7 @@ export class CategoriesComponent implements OnInit {
         loading: true,
       },
     };
+    //MANZA PUTO
     this.categories = new Map<number, Category>();
     this.createCategoryFormControls();
     this.createSubcategoryFormControls();
