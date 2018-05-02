ca7deca7a26beaf3176ed923b67bbe763b22bc8e
LucasManza <manzanelli95lucas@gmail.com>
2018-05-02 09:53:22 -0300

    upload-product
    Modification.

--- ui/src/app/user/upload-product/upload-product.component.ts
+++ ui/src/app/user/upload-product/upload-product.component.ts
@@ -1,9 +1,7 @@
 import {Component, OnInit, TemplateRef} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {BsModalRef} from "ngx-bootstrap/modal/bs-modal-ref.service";
 import {Router} from "@angular/router";
 import {Title} from "@angular/platform-browser";
-import {BsModalService} from "ngx-bootstrap/modal";
 import {ProductService} from "../../shared/services/product.service";
 import {Product} from "../../shared/models/product.model";
 import {Subcategory} from "../../shared/models/subcategory.model";
@@ -12,129 +10,25 @@ import {SubcategoryService} from "../../shared/services/subcategory.service";
 import {CategoryService} from "../../shared/services/category.service";
 import {SupplierService} from "../../shared/services/supplier.service";
 import {Supplier} from "../../shared/models/supplier.model";
+import {UserAuthService} from "../../shared/auth/user/user-auth.service";
 
 
 @Component({
   selector: 'app-upload-product',
   templateUrl: './upload-product.component.html',
   styleUrls: ['./upload-product.component.scss'],
-  providers: [ProductService, SubcategoryService, CategoryService, SupplierService]
+  providers: [ProductService, SubcategoryService, CategoryService, SupplierService, UserAuthService]
 })
 export class UploadProductComponent implements OnInit {
 
-  //
-  // public productFormGroup: FormGroup;
-  // public newProduct: Product;
-  // public productToDelete: Product;
-  // public productIndexToDelete: number;
-  // public productIndexUpdate: number;
-  // public products: Product[];
-  // public alerts: {
-  //   addProduct: {
-  //     error: boolean,
-  //     loading: boolean,
-  //   },
-  //   products: {
-  //     error: boolean,
-  //     loading: boolean,
-  //     deleting: boolean,
-  //     deletingError: boolean,
-  //   },
-  // };
-  // modalRef: BsModalRef;
-  //
-  // constructor(public fb: FormBuilder, public productService: ProductService, public router: Router, private titleService: Title, private modalService: BsModalService) {
-  // }
-  //
-  // ngOnInit() {
-  //   this.titleService.setTitle('AM Products | Stingy');
-  //   this.newProduct = Product.empty();
-  //   this.alerts = {
-  //     addProduct: {
-  //       error: false,
-  //       loading: false,
-  //     },
-  //     products: {
-  //       error: false,
-  //       loading: true,
-  //       deleting: false,
-  //       deletingError: false
-  //     },
-  //   };
-  //   this.createProductFormControls();
-  //   this.getProducts();
-  // }
-  //
-  // getProducts() {
-  //   this.productService.products.then(res => {
-  //     this.products = res;
-  //     this.alerts.products.error = false;
-  //     this.alerts.products.loading = false;
-  //   }).catch(err => {
-  //     //TODO mostrar en el front mensaje de error
-  //     this.alerts.products.error = true;
-  //     this.alerts.products.loading = false;
-  //   })
-  // }
-  //
-  //
-  // uploadProduct() {
-  //   this.alerts.addProduct.loading = true;
-  //   if(this.newProduct.id) {
-  //     this.newProduct.id = parseInt(String(this.newProduct.id));
-  //     this.productService.updateProduct(this.newProduct).then(res => {
-  //       this.products[this.productIndexUpdate] = res;
-  //       this.alerts.addProduct.loading = false;
-  //       this.alerts.addProduct.error = false;
-  //       this.newProduct = Product.empty();
-  //       this.productFormGroup.reset();
-  //       this.modalRef.hide();
-  //     }).catch(() => {
-  //       this.alerts.addProduct.loading = false;
-  //       this.alerts.addProduct.error = true;
-  //     })
-  //   } else {
-  //     this.newProduct.id = parseInt(String(this.newProduct.id));
-  //     this.productService.addProduct(this.newProduct).then(res => {
-  //       this.products.push(res);
-  //       this.alerts.addProduct.loading = false;
-  //       this.alerts.addProduct.error = false;
-  //       this.newProduct = Product.empty();
-  //       this.productFormGroup.reset();
-  //       this.modalRef.hide();
-  //     }).catch(() => {
-  //       this.alerts.addProduct.loading = false;
-  //       this.alerts.addProduct.error = true;
-  //     })
-  //   }
-  // }
-  //
-  // private createProductFormControls() {
-  //   this.productFormGroup = this.fb.group({
-  //     name: ['', Validators.required],
-  //     categoryId: [Number, Validators.required],
-  //   })
-  // }
-  //
-  // openProductModal(template: TemplateRef<any>, product?) {
-  //   if(product) this.newProduct = product;
-  //   this.modalRef = this.modalService.show(template);
-  // }
-  //
-  // resetDeleteModal(){
-  //   this.productIndexToDelete = -1;
-  //   this.productToDelete = undefined;
-  // }
-
   public formGroup: FormGroup;
+  public userId: number;
   public newProduct: Product;
   public uploadProductError: boolean;
   public addingProduct: boolean;
   public selectedCategoryId: number;
   public categories: Category[];
-  public selectedSubcategoryId: number;
   public subcategories: Subcategory[];
-  public selectedSupplierId: number;
   public suppliers: Supplier[];
   public alerts: {
     subcategories:{
@@ -154,7 +48,7 @@ export class UploadProductComponent implements OnInit {
 
   constructor(public fb: FormBuilder, public productService: ProductService, public router: Router, private titleService: Title,
               private subcategoryService: SubcategoryService, private categoryService: CategoryService,
-              private supplierService: SupplierService
+              private supplierService: SupplierService, private userAuthService: UserAuthService
   ) {}
 
   ngOnInit() {
@@ -162,6 +56,7 @@ export class UploadProductComponent implements OnInit {
     this.newProduct = Product.empty();
     this.addingProduct = false;
     this.uploadProductError = false;
+    this.userAuthService.loggedUser.then(res => this.userId = res.id);
     this.createFormControls();
     this.subcategories = [];
     this.getCategories();
@@ -185,17 +80,14 @@ export class UploadProductComponent implements OnInit {
 
   uploadProduct() {
     this.addingProduct = true;
-    if(this.selectedSupplierId == null && this.selectedSubcategoryId == null){
-      this.uploadProductError = true;
-      this.addingProduct = false;
-      return;
-    }
     this.newProduct.isValidated = false;
-    this.newProduct.subcategoryId = this.selectedSubcategoryId;
-    this.newProduct.supplierId = this.selectedSupplierId;
+    this.newProduct.userId = this.userId;
+    this.newProduct.subcategoryId = parseInt(String(this.newProduct.subcategoryId));
+    this.newProduct.supplierId = parseInt(String(this.newProduct.supplierId));
     this.productService.addProduct(this.newProduct).then( () => {
       this.addingProduct = false;
       this.uploadProductError = false;
+      this.resetUploadProduct();
     }).catch(() => {
       this.uploadProductError = true;
       this.addingProduct = false;
@@ -209,6 +101,8 @@ export class UploadProductComponent implements OnInit {
       price: ['', [Validators.required,Validators.min(0)]],
       description: ['', Validators.required],
       imageUrl: ['', [Validators.required,]],
+      subcategoryId: [-1, [Validators.required,Validators.min(0)]],
+      supplierId: [-1, [Validators.required,Validators.min(0)]],
     }, {
     })
   }
@@ -247,6 +141,12 @@ export class UploadProductComponent implements OnInit {
       this.alerts.suppliers.error = true;
       this.alerts.suppliers.loading = false;
     })
+  }
+
+  resetUploadProduct(){
+    this.formGroup.reset();
+    this.newProduct = Product.empty();
+    this.selectedCategoryId = -1;
   }
 
 }
